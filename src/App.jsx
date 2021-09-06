7db91e2c85853d3f5c084e0dce275dacf82457b2
strickerkarin <strickerkarin@hotmail.com>
2021-09-06 17:10:04 -0300

    Inicio evaluación - Agrego estilos

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <div className="App">      
+      <div className="layout">Este es el layout
+        <h1 className="titulo">Elige tu propia aventura</h1>
+        <div className="historia">Acá iria la historia</div>
+        <div className="opciones">Acá estarían las opciones</div>
+        <div className="opcion">Y acá cada opción</div>
+        <div className="recordatorio">Este es un recordatorio</div>
+        <div className="botones">Estos son botones</div>
+        <div className="botones">Estos son botones</div>
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
