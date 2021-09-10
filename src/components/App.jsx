2beed44d8f418f2ec03a76711f5ab3a60ec8442c
strickerkarin <strickerkarin@hotmail.com>
2021-09-10 15:52:22 -0300

    [FIX] SAco la función devolver objeto

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,24 +15,18 @@ export default class App extends Component {
   UNSAFE_componentWillMount = () => {
     this.setState({ pagina: 0, objetoActual: data[0] })
 
-  }  
-
-  devolverObjeto = (idBuscado) => {
-    let objeto = null
-    for (let i = 0; i < data.length; i++) {
-      if (data[i].id === idBuscado) {
-        objeto = data[i]
-      }
-    }
-    return objeto
   }
 
   handleClick = (evento) => {
     const id = evento.target.id;
-    this.setState({ objetoActual: this.devolverObjeto(`${this.state.pagina + 2}${id}`), opcionAnterior: id.toUpperCase() })
-    
-    if (this.state.pagina < 4) {
-      this.setState({ pagina: this.state.pagina + 1 })
+    if (this.state.pagina == 4) {
+      alert("FIN");
+    } else {
+      this.setState({
+        pagina: this.state.pagina + 1,
+        objetoActual: data.filter(e => e.id === `${this.state.pagina + 2}${id}`)[0],
+        opcionAnterior: id.toUpperCase()
+      })
     }
   }
 
@@ -41,31 +35,25 @@ export default class App extends Component {
   }
 
   render() {
-    if (this.state.objetoActual != null) {
-      return (
-        <div className="App">
-          <div className="layout">
-            <h1 className="titulo">Elige tu propia aventura</h1>
-            <h2 className="historia">{this.state.objetoActual.historia}</h2>
-            <Opciones
-              handleClick={this.handleClick}
-              opcionA={this.state.objetoActual.opciones.a}
-              opcionB={this.state.objetoActual.opciones.b}
-            />
-            <Recordatorio
-              seleccionAnterior={this.state.opcionAnterior}
-              historial={historial.map((opcion, i) =>
-                (<li key={i}>{opcion}</li>)
-              )}
-            />
-          </div>
+    return (
+      <div className="App" >
+        <div className="layout">
+          <h1 className="titulo">Elige tu propia aventura</h1>
+          <h2 className="historia">{this.state.objetoActual.historia}</h2>
+          <Opciones
+            handleClick={this.handleClick}
+            opcionA={this.state.objetoActual.opciones.a}
+            opcionB={this.state.objetoActual.opciones.b}
+          />
+          <Recordatorio
+            seleccionAnterior={this.state.opcionAnterior}
+            historial={historial.map((opcion, i) =>
+              (<li key={i}>{opcion}</li>)
+            )}
+          />
         </div>
-      );
-    } else {
-      alert("FIN");
-    }
-
+      </div>
+    );
   }
-
 }
 
